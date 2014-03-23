e35b7feaf9f7d7c606a1c60efb7a018d93b78a40
Laurent Peuch <cortex@worlddomination.be>
2014-03-23 08:25:33 +0100

    [enh] add a way to convert back a RedBaron instance insto fst

--- redbaron/redbaron.py
+++ redbaron/redbaron.py
@@ -3,6 +3,10 @@ from UserList import UserList
 
 import nodes
 
+
 class RedBaron(UserList):
     def __init__(self, source_code):
         self.data = map(nodes.to_node, baron.parse(source_code))
+
+    def __fst__(self):
+        return [x.__fst__() for x in self.data]
