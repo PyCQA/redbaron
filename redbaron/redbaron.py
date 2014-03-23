d6e2a1c039974c0745967a5a2247f07940bdd299
Laurent Peuch <cortex@worlddomination.be>
2014-03-23 05:12:12 +0100

    init

---
+++ redbaron/redbaron.py
@@ -0,0 +1,3 @@
+class RedBaron(object):
+    def __init__(self, source_code):
+        pass
---
+++ redbaron/test_redbaron.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+
+from redbaron import RedBaron
+
+
+def test_empty():
+    RedBaron("")
