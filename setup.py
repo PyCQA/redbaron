7ebbffdffcf227cccc64d44321d71483de6bd3e4
Laurent Peuch <cortex@worlddomination.be>
2014-03-25 03:57:02 +0100

    [enh] add some setup.py (that will not work for now since baron 0.1.2 is not released yet)

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='redbaron',
+      version='0.1',
+      description='Abstraction on top of baron, a FST for python to make writing refactoring code a realistic task',
+      author='Laurent Peuch',
+      #long_description='',
+      author_email='cortex@worlddomination.be',
+      url='https://github.com/Psycojoker/redbaron',
+      install_requires=['baron>=0.1.2'],
+      license= 'lgplv3+',
+      keywords='baron fst ast refactoring',
+     )
