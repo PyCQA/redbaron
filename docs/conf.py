95377fd0f81bb82c66c95a4f8844622e4513a843
Laurent Peuch <cortex@worlddomination.be>
2014-03-28 03:29:50 +0100

    [doc/enh] use the awesome ipython directive

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.todo',
+    'IPython.sphinxext.ipython_directive',
+    'IPython.sphinxext.ipython_console_highlighting',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
