db45e0d740964b52588674a271f6e0ba84f83d91
Laurent Peuch <cortex@worlddomination.be>
2014-09-22 05:05:35 +0200

    [enh] put CHANGELOG in setup.py and do the magic with pypandoc

--- setup.py
+++ setup.py
@@ -3,11 +3,19 @@
 
 from setuptools import setup
 
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+
 setup(name='redbaron',
       version='0.2',
       description='Abstraction on top of baron, a FST for python to make writing refactoring code a realistic task',
       author='Laurent Peuch',
-      long_description=open("README.md", "r").read(),
+      long_description=read_md("README.md") + "\n\n" + open("CHANGELOG", "r").read(),
       author_email='cortex@worlddomination.be',
       url='https://github.com/Psycojoker/redbaron',
       install_requires=['baron>=0.3.1', 'pygments'],
