13d35ddd5813e3e75ad4932334bca90122a75441
Pierre Penninckx <pierre.penninckx@spacebel.be>
2015-01-23 14:20:11 +0100

    [mod] extract rendering tests in own module

--- tests/test_redbaron.py
+++ tests/test_redbaron.py
@@ -19,31 +19,6 @@ def test_index():
     assert red[0].value.index_on_parent is None
 
 
-def test_rendering_iter():
-    red = RedBaron("a + 2")
-    assert list(red._generate_nodes_in_rendering_order()) == [red[0], red.name, red[0].first_formatting[0], red[0], red[0].second_formatting[0], red.int]
-    assert list(red[0]._generate_nodes_in_rendering_order()) == [red[0], red.name, red[0].first_formatting[0], red[0], red[0].second_formatting[0], red.int]
-
-
-def test_next_rendered():
-    red = RedBaron("a + 2")
-    f = red.name
-
-    assert f.next_rendered is red[0].first_formatting[0]
-    assert f.next_rendered.next_rendered is red[0]
-    assert f.next_rendered.next_rendered.next_rendered is red[0].second_formatting[0]
-    assert f.next_rendered.next_rendered.next_rendered.next_rendered is red.int
-
-
-def test_previous_rendered():
-    red = RedBaron("a + 2")
-    f = red.int
-
-    assert f.previous_rendered is red[0].second_formatting[0]
-    assert f.previous_rendered.previous_rendered is red[0]
-    assert red[0].first_formatting[0].previous_rendered is red.name
-
-
 test_indent_code = """
 def a():
     # plop
@@ -53,10 +28,6 @@ def a():
     pouf
 
 """
-
-def test_next_rendered_trapped():
-    red = RedBaron(test_indent_code)
-    assert red("endl")[5].next_rendered is red.find("name", "pouf")
 
 
 def test_increase_indentation():
