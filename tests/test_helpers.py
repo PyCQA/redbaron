a1599c590d09f013b217a84c0a1bb6d6ea8dceb1
Laurent Peuch <cortex@worlddomination.be>
2015-01-29 12:17:51 +0100

    [enh] test import .names helper

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -4,3 +4,8 @@ from redbaron import RedBaron
 def test_import_modules():
     red = RedBaron("import a, b.c, d.e as f")
     assert red[0].modules() == ['a', 'b.c', 'd.e']
+
+
+def test_import_names():
+    red = RedBaron("import a, b.c, d.e as f")
+    assert red[0].names() == ['a', 'b.c', 'f']
