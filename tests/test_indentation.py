c6161cfb094921e6b43f16849ae8184561300cf7
Laurent Peuch <cortex@worlddomination.be>
2015-05-05 10:39:51 +0200

    [fix] missing tests for individual node increase/decrease indentations

--- tests/test_indentation.py
+++ tests/test_indentation.py
@@ -29,3 +29,15 @@ def test_decrease_indentation():
     red.decrease_indentation(4)
     indented_code = "\ndef a():\n" + "\n".join(map(lambda x: x[4:], test_indent_code.split("\n")[2:-2])) + "\n\n"
     assert red.dumps() == indented_code
+
+
+def test_increase_indentation_single_node():
+    red = RedBaron(test_indent_code)
+    red.if_.value[0].increase_indentation(3)
+    assert len(red.if_.value[0].indentation) == 8 + 3
+
+
+def test_decrease_indentation_single_node():
+    red = RedBaron(test_indent_code)
+    red.if_.value[0].decrease_indentation(3)
+    assert len(red.if_.value[0].indentation) == 5
