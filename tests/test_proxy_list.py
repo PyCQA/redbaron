f076364fc2353acd1bc90a1f08d5dd32aabc4c77
Laurent Peuch <cortex@worlddomination.be>
2015-09-03 10:35:53 +0200

    [mod] looks like a reasonable behavior actually

--- tests/test_proxy_list.py
+++ tests/test_proxy_list.py
@@ -1131,7 +1131,7 @@ def test_decorator_line_proxy_dont_break_next_block_identation():
 def test_line_proxy_correctly_indent_code_block():
     red = RedBaron("while True:\n    pass\n")
     red[0].extend(["if a:\n    pass\n\n"])
-    assert red.dumps() == "while True:\n    pass\n    if a:\n        pass\n"
+    assert red.dumps() == "while True:\n    pass\n    if a:\n        pass\n\n"
 
 
 def test_root_as_line_proxy_list_len():
