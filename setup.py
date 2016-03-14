70dbea1cfbcbcd4b9e5a3ee8fe15aa8096effd34
Laurent Peuch <cortex@worlddomination.be>
2016-03-14 03:45:45 +0100

    [mod] I need latest baron version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name='redbaron',
       long_description=read_md("README.md") + "\n\n" + open("CHANGELOG", "r").read(),
       author_email='cortex@worlddomination.be',
       url='https://github.com/Psycojoker/redbaron',
-      install_requires=['baron>=0.6.1'],
+      install_requires=['baron>=0.6.2'],
       extras_require={
           "notebook": ["pygments"],
       },
