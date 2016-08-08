4b72bad73b1629671200af6056908927b8638ea0
bey <mehty.musaev@gmail.com>
2016-08-08 13:25:40 +0300

    Add more tests for at method. Fix bug. Update CHANGELOG and documentation.

--- tests/test_at.py
+++ tests/test_at.py
@@ -4,11 +4,11 @@
 
 import pytest
 import redbaron
-from redbaron import RedBaron, DecoratorNode, AssignmentNode, DefNode, EndlNode
+from redbaron import RedBaron
 
 redbaron.DEBUG = True
 
-fst = RedBaron("""\
+fst_def = RedBaron("""\
 @deco
 
 def foo(a, b):
@@ -17,9 +17,26 @@ def foo(a, b):
 """)
 
 
-def test_at():
-    assert isinstance(fst.at(1), DecoratorNode) is True
-    assert isinstance(fst.at(2), RedBaron) is True
-    assert isinstance(fst.at(3), DefNode) is True
-    assert isinstance(fst.at(4), AssignmentNode) is True
-    assert isinstance(fst.at(5), AssignmentNode) is True
+def test_at_def():
+    assert fst_def.at(1) is fst_def.find('DecoratorNode')
+    assert fst_def.at(2) is fst_def
+    assert fst_def.at(3) is fst_def.def_
+    assert fst_def.at(4) is fst_def.find_all('AssignmentNode')[0]
+    assert fst_def.at(5) is fst_def.find_all('AssignmentNode')[1]
+
+
+fst_class = RedBaron("""\
+class Foo(object):
+    def __init__(self):
+        self.a = None
+    def bar(self):
+        return self.a + 5
+""")
+
+
+def test_at_class():
+    assert fst_class.at(1) is fst_class.class_
+    assert fst_class.at(2) is fst_class.find_all('DefNode')[0]
+    assert fst_class.at(3) is fst_class.find('AssignmentNode')
+    assert fst_class.at(4) is fst_class.find_all('DefNode')[1]
+    assert fst_class.at(5) is fst_class.find('ReturnNode')
