90c212f81a5c483d5c279409f86b6ff954e2e743
bey <mehty.musaev@gmail.com>
2016-08-08 16:46:21 +0300

    Remove useful tests. Make code clear.

--- tests/test_at.py
+++ tests/test_at.py
@@ -8,48 +8,20 @@ from redbaron import RedBaron
 
 redbaron.DEBUG = True
 
-fst_def = RedBaron("""\
-@deco
-
-def foo(a, b):
-    c = a + b
-    e = 1
-""")
-
-
-def test_at_def():
-    assert fst_def.at(1) is fst_def.find('DecoratorNode')
-    assert fst_def.at(2) is fst_def
-    assert fst_def.at(3) is fst_def.def_
-    assert fst_def.at(4) is fst_def.find('NameNode', value='c')
-    assert fst_def.at(5) is fst_def.find('NameNode', value='e')
-
-
-fst_class = RedBaron("""\
+red = RedBaron("""\
 class Foo(object):
     def __init__(self):
         self.a = None
     def bar(self):
-        return self.a + 5
+        for x in range(5):
+            yield self.a + x
 """)
 
 
-def test_at_class():
-    assert fst_class.at(1) is fst_class.class_
-    assert fst_class.at(2) is fst_class.find_all('DefNode')[0]
-    assert fst_class.at(3) is fst_class.find('AtomtrailersNode')
-    assert fst_class.at(4) is fst_class.find_all('DefNode')[1]
-    assert fst_class.at(5) is fst_class.find('ReturnNode')
-
-
-fst_simple = RedBaron("""\
-a = 5
-
-b = 6
-""")
-
-
-def test_at_simple():
-    assert fst_simple.at(1) is fst_simple.find_all('NameNode')[0]
-    assert fst_simple.at(2) is fst_simple
-    assert fst_simple.at(3) is fst_simple.find_all('NameNode')[1]
+def test_at():
+    assert red.at(1) is red.class_
+    assert red.at(2) is red.find_all('DefNode')[0]
+    assert red.at(3) is red.find('AtomtrailersNode')
+    assert red.at(4) is red.find_all('DefNode')[1]
+    assert red.at(5) is red.find('ForNode')
+    assert red.at(6) is red.find('YieldNode')
