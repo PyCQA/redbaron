6c3e17e8233274b6f002b0dcb16838965a59d050
bey <mehty.musaev@gmail.com>
2016-08-08 14:37:19 +0300

    Optimize at method. Add more tests.

--- tests/test_at.py
+++ tests/test_at.py
@@ -21,8 +21,8 @@ def test_at_def():
     assert fst_def.at(1) is fst_def.find('DecoratorNode')
     assert fst_def.at(2) is fst_def
     assert fst_def.at(3) is fst_def.def_
-    assert fst_def.at(4) is fst_def.find_all('AssignmentNode')[0]
-    assert fst_def.at(5) is fst_def.find_all('AssignmentNode')[1]
+    assert fst_def.at(4) is fst_def.find('NameNode', value='c')
+    assert fst_def.at(5) is fst_def.find('NameNode', value='e')
 
 
 fst_class = RedBaron("""\
@@ -37,6 +37,19 @@ class Foo(object):
 def test_at_class():
     assert fst_class.at(1) is fst_class.class_
     assert fst_class.at(2) is fst_class.find_all('DefNode')[0]
-    assert fst_class.at(3) is fst_class.find('AssignmentNode')
+    assert fst_class.at(3) is fst_class.find('AtomtrailersNode')
     assert fst_class.at(4) is fst_class.find_all('DefNode')[1]
     assert fst_class.at(5) is fst_class.find('ReturnNode')
+
+
+fst_simple = RedBaron("""\
+a = 5
+
+b = 6
+""")
+
+
+def test_at_simple():
+    assert fst_simple.at(1) is fst_simple.find_all('NameNode')[0]
+    assert fst_simple.at(2) is fst_simple
+    assert fst_simple.at(3) is fst_simple.find_all('NameNode')[1]
