23a26cd5560958f36f80157c017382101536cd24
BugDiver <vinaysh@thoughtworks.com>
2018-07-12 12:06:25 +0530

    Using new pattern field from re module #164

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 
 setup(name='redbaron',
-      version='0.6.3',
+      version='0.6.4',
       description='Abstraction on top of baron, a FST for python to make writing refactoring code a realistic task',
       author='Laurent Peuch',
       long_description=read_md("README.md") + "\n\n" + open("CHANGELOG", "r").read(),
